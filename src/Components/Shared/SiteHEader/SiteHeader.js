1b184659337d670cfe9fcedc04eee5faab0aca47
jaspia-1 <aspia2001.icloud@gmail.com>
2022-10-27 10:13:02 +0600

    navbar done

---
+++ src/Components/Shared/SiteHEader/SiteHeader.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link, NavLink } from 'react-router-dom';
+import ToggleSwitch from './ToggleSwitch';
+
+const SiteHeader = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Link to='/' className="fw-bold navbar-brand" href="#home"><span style={{ "color": "rgb(194, 9, 194)" }}>S</span>hi<span style={{ "color": "gold" }}>kho</span></Link>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <ToggleSwitch className="" label="theme" ></ToggleSwitch>
+                            <NavLink className="nav-link" to="/allcourses">Courses</NavLink>
+                            <NavLink className="nav-link" to="/faq">FAQ</NavLink>
+                            <NavLink className="nav-link" to="/blog">Blog</NavLink>
+
+                        </Nav>
+                        <Nav>
+
+
+
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default SiteHeader;
--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import SiteHeader from '../Components/Shared/SiteHEader/SiteHeader';
 
 const Main = () => {
     return (
         <div>
-
+            <SiteHeader></SiteHeader>
+            <Outlet></Outlet>
         </div>
     );
 };
