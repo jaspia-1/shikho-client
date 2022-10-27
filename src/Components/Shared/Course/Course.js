3fd6b97c62c573cb5d80890fb6f82bca9cf0ab43
jaspia-1 <aspia2001.icloud@gmail.com>
2022-10-27 13:38:06 +0600

    card done

--- src/Components/Shared/Course/Course.js
+++ src/Components/Shared/Course/Course.js
@@ -1,38 +1,11 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { FaStar, FaRegClock, FaMoneyBillAlt } from 'react-icons/fa';
 
 const Course = ({ course }) => {
     const { catagory, duration, id, img, name, price, rating, title } = course;
     return (
-        // <div >
-        //     <div className=' h-100 m-2 card rounded'>
-        //         <div className='card-body'>
-
-
-        //             <div className='w-100'>
-        //                 <img src={img} style={{ width: "520px " }} className="img-fluid" alt="" />
-        //             </div>
-        //             <div className='w-100 p-2  '>
-        //                 <h5 className='text-center mt-2 fw-bolder '>{title}</h5>
-        //                 <div className='text-start ms-2'>
-
-        //                     <p className='m-0 text-end' >{name}</p>
-        //                     <p >{rating}</p>
-        //                     <div className='d-flex justify-content-between m-0'>
-        //                         <p className='gray-text m-0' ><small> {duration}</small></p>
-        //                         <p className='gray-text  ' ><small>  ${price}</small></p>
-
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //             <footer className=' card-footer text-muted '>
-        //                 <Link to={`/course/${id}`}><button className='btn btn-outline-primary mb-0 w-100 my-auto'>Go Details</button></Link>
-
-        //             </footer>
-        //         </div>
-        //     </div>
-        // </div>
 
         <div>
             <div class="card text-center rounded h-100 shadow">
@@ -43,10 +16,10 @@ const Course = ({ course }) => {
                     <div className='text-start ms-2'>
 
                         <p className='m-0 text-end text-secondary' >{name}</p>
-                        <p >{rating}</p>
+                        <p ><FaStar className='text-warning me-2  ' />{rating}</p>
                         <div className='d-flex justify-content-between m-0'>
-                            <p className='gray-text m-0' ><small> {duration}</small></p>
-                            <p className='gray-text  ' ><small>  ${price}</small></p>
+                            <p className='gray-text m-0' ><small> <FaRegClock className='text-warning me-2 ' />{duration}</small></p>
+                            <p className='gray-text  ' ><small><FaMoneyBillAlt className='text-success me-2 ' />  ${price}</small></p>
 
                         </div>
                     </div>
