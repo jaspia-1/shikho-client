1d4905e91d1148a18d69ec87ab12876e2aaeb6bb
jaspia-1 <aspia2001.icloud@gmail.com>
2022-10-27 10:45:29 +0600

    HOme page done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import './Home.css'
 
 const Home = () => {
@@ -9,6 +10,25 @@ const Home = () => {
                     <h1 className='p-2 fw-bold text-white display-3 '>Grab Your Courses & grow your skill</h1>
                     <div>
                         <img src='/assets/header.png' alt="" />
+                    </div>
+
+                </div>
+            </div>
+
+            <div>
+                <Container>
+                    <h1 className="p-3 fw-bold my-3">We collaborate with <span className='text-danger'> 275+ </span>leading  <br />universities and companies</h1>
+
+
+                </Container>
+                <div className='company-bg p-3'>
+                    <div className='d-flex container py-3   justify-content-evenly'>
+                        <img className='img-fluid' style={{ height: "50px" }} src="/assets/company/cimg1.png" alt="" />
+                        <img className='img-fluid' style={{ height: "50px" }} src="/assets/company/cimg2.png" alt="" />
+                        <img className='img-fluid' style={{ height: "50px" }} src="/assets/company/cimg3.png" alt="" />
+                        <img className='img-fluid' style={{ height: "50px" }} src="/assets/company/cimg4.png" alt="" />
+                        <img className='img-fluid' style={{ height: "50px" }} src="/assets/company/cimg5.png" alt="" />
+
                     </div>
 
                 </div>
